cf07d87ce771beb6047cbe08382e483249520565
lbs145956 <1459569207@qq.com>
2023-05-23 18:37:28 +0800

    注释

---
+++ test5_19/fuxi.cpp
@@ -0,0 +1,106 @@
+ #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//const修饰的常变量
+//const int num = 10;
+//int	main()
+//{
+//	int num = 20;
+//	printf("%d", num);
+//	return 0;
+//}
+
+//字符串
+//int main()
+//{
+//	char arr1[] = { "abc" };
+//	char arr2[] = { 'a','b','c',0 };
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+
+//计算字符串长度
+//int main()
+//{
+//	char arr1[] = { "abc" };
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	printf("%c\n", '\32');
+//	printf("%d\n", strlen("c:\test\32\test.c"));
+//	return 0;
+//}
+
+//转义字符
+//int main()
+//{
+//	printf("%c\n", '\32');
+//	printf("%c\n", '\x32');
+//	printf("%c\n", '\'');
+//	printf("%c\n", '\"');
+//	printf("%c\n", '\?');
+//	printf("%c\n", '\\');
+//
+//	return 0;
+//}
+
+
+//选择语句
+//int main()
+//{
+//	int input = 0;
+//	printf("我会坚持学习吗？（1/0）: ");
+//	scanf("%d", &input);
+//	if (input==1)
+//	{
+//		printf("考研成功！");
+//	}
+//	else
+//	{
+//		printf("考研失败！");
+//	}
+//	return 0;
+//}
+
+//循坏语句
+
+//int main()
+//{
+//	int line = 0;
+//	printf("学习c语言\n");
+//	while (line<5000)
+//	{
+//		line++;
+//		printf("敲一行代码：%d\n", line);
+//		
+//	}
+//	printf("学习成功！");
+//	return 0;
+//}
+
+//函数 自定义函数：输入两数求和
+//int add(int	x,int	y)
+//{
+//	int	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int sum;
+//	int num1;
+//	int num2;
+//	printf("输入两个数：");
+//	scanf("%d %d", &num1, &num2);
+//	sum = add(num1, num2);
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+
+
+
---
+++ test5_19/test.cpp
@@ -0,0 +1,96 @@
+ //#define _CRT_SECURE_NO_WARNINGS
+//常量：字面常量  const修饰的常变量 #define定义的标识符常量 枚举常量（enum）
+//字符:字符串"abc" 'a' '\0'(结束的标志)   strlen（）（计算字符串长度） 
+        //转义字符
+//#include <stdio.h>
+//#include<string.h>
+
+
+
+
+
+
+
+
+
+//字符串：
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = {'a','b','c',0};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+//计算字符串长度：
+//#include<string.h>
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+
+//转义字符
+//   1 \ddd 8进制   \132
+//   2 \xdd 16进制  \x23
+//int main()
+//{
+//
+//   
+//    printf("%c\n", '\32');
+//    //printf("%d\n", strlen("c:\test\32\test.c"));
+//    return 0;
+//}
+
+//选择语句
+//int main()
+//{
+//    int input = 0;
+//    printf("我会坚持学习下去吗？（1/0）：");
+//    scanf("%d", &input);
+//    if (input == 1)
+//        printf("考研成功！");
+//    else
+//        printf("考研失败");
+//    return 0;
+
+//}
+
+
+//循环语句
+//while循环
+//例子:如果敲了20000行代码，就能获得一个好offer。
+
+//int main()
+//{
+//    int line = 0;
+//    printf("开始学习");
+//    while (line <20000)
+//    {
+//        line++;
+//        printf("敲一行代码：%d\n", line);
+//    }
+//    printf("学习成功");
+//    return 0;
+//
+//}
+
+
+//函数:自定义函数；库函数
+//两数相加
+  
+
+//数组: int arr[10]={0,1,2,3,4,5,6,7,8,9}; arr[4]:找数组的第五个元素
+
+
+
+
+
+
+
+
+
