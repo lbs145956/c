02b4a679585362a46145008ca1ef90cc0b05fb98
lbs145956 <1459569207@qq.com>
2023-05-23 18:43:54 +0800

    注释

---
+++ test5_20/fuxi.cpp
@@ -0,0 +1,52 @@
+ #define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+// 按位与（&）1&0=0 1&1=1 0&0=0 1010(10)&0110(6)=0010(2)
+//int main()
+//{
+//	int a = 10;
+//	int b = 6;
+//	int c = 0;
+//	c = a & b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//按位或（|） 1|0=1 1010|0110=1110
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 6;
+//	int c = 0;
+//	c = a | b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//按位异或(^) 1^0=1 1^1=0 0^0=0 1010^0110=1100
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 6;
+//	int c = 0;
+//	c = a ^ b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+
+//赋值操作符 （&=，+=）a+=6>>a=a+6
+//int main()
+//{
+//	int a = 10;
+//	a |= 6;
+//	printf("%d", a);
+//	return 0;
+//}
+
+//单目操作符
+
+
---
+++ test5_20/test.cpp
@@ -0,0 +1,23 @@
+ #define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+//按位操作符：1.按位与（&）：011 & 101  = 001
+          //2.按位或（|）：011 | 101  = 111 
+          //3.按位异或 （^）：011 ^ 101 = 110
+//
+//int main()
+//{
+//    int a = 3;
+//    int b = 5;
+//    int num1 = 0;
+//    int num2 = 0;
+//    int num3 = 0;
+//    num1 = a & b;
+//    num2 = a | b;
+//    num3 = a ^ b;
+//    printf("%d  %d  %d \n", num1, num2, num3);
+//    return 0;
+//}
+
+//赋值操作符
+//单目操作符
